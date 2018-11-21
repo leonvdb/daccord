f58bc5b936cb517499169265364caec23754deda
Leon von dem Berge <leon.vondemberge@gmail.com>
2018-11-21 18:07:08 +0200

    Set up Schemas #2
    -Created basic Poll and Option schemas
    -POST requests to /api/options add options to DB

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 //Import API Routes
 const testRoute = require('./routes/api/test'); //To be deleted after review
+const options = require('./routes/api/options');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/systemic-consensys')
@@ -16,6 +22,7 @@ app.get('/', (req, res) => res.send("Test"));
 
 //Use Routes
 app.use('/api/test', testRoute); //To be deleted after review
+app.use('/api/options', options);
 
 // Setting port for server
 const port = 5000;
