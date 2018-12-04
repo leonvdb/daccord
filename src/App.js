cd0120ecf9e2aaa23141bcbed19f37d00fe36e22
Leon von dem Berge <leon.vondemberge@gmail.com>
2018-12-04 13:39:03 +0200

    Set up NotFound.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -8,6 +8,7 @@ import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Poll from './components/poll/Poll';
 import CreatePoll from './components/CreatePoll';
+import NotFound from './components/NotFound';
 
 import './App.css';
 
@@ -18,9 +19,12 @@ class App extends Component {
         <BrowserRouter>
           <div className="App">
             <Header />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/poll/:poll_id" component={Poll} />
-            <Route exact path="/create" component={CreatePoll} />
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/poll/:poll_id" component={Poll} />
+              <Route exact path="/create" component={CreatePoll} />
+              <Route component={NotFound} />
+            </Switch>
             <Footer />
           </div>
         </BrowserRouter>
