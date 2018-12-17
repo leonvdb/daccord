91acaeda472e9149de0dc3144532f165fc58ce03
Leon von dem Berge <leon.vondemberge@gmail.com>
2018-12-17 17:53:27 +0200

    Initial implementation of Vote
    TODO: Fix bug - Users votes get added multiple times instead of editing existing vote.

--- src/server/routes/api/polls.ts
+++ src/server/routes/api/polls.ts
@@ -117,4 +117,60 @@ router.delete('/:poll_id', (req, res) => {
 });
 
 
+//@route    PUT api/polls/:poll_id/vote
+//@desc     Vote on options
+//@access   Private // TODO: Make route private
+//@params   UserEmail(could also be ID), {option.RefID: Vote,}
+
+router.put('/:poll_id/vote', (req, res) => {
+    Poll.findOne({ refId: req.params.poll_id })
+        .then((poll: IPollModel) => {
+            //Check if poll exists
+            if (!poll) return res.status(404).json({ 'msg': 'There is no poll for this ID' });
+            //Check if user exists
+            User.findOne({ email: req.body.email }).then((user: IUserModel) => {
+                if (user) {
+                    // Create new poll with user.id
+                    vote(user);
+                } else {
+                    //No corresponding user - Create new User
+                    const newUser = new User({
+                        email: req.body.email
+                    });
+
+                    //Save new User and create new poll with user.id
+                    newUser
+                        .save()
+                        .then((user: IUserModel) => vote(user))
+                }
+
+                function vote(user: IUserModel) {
+
+                    for (let i = 0; i < poll.options.length; i++) {
+                        //Check if Option has been voted on in this request
+                        if (!(req.body.votes.hasOwnProperty(poll.options[i].refId))) {
+                            continue;
+                        }
+                        //Check if this Option has already been voted on by this User
+
+
+                        //Construct vote for option
+                        const votePayload = req.body.votes[poll.options[i].refId];
+                        const newVote = {
+                            voter: user._id,
+                            vote: votePayload
+                        }
+                        poll.options[i].votes.unshift(newVote)
+                    }
+                }
+                //Save
+                poll.save().then(poll => res.json(poll));
+            })
+                .catch((err: Error) => res.json(err));
+
+
+        })
+})
+
+
 export default router;
