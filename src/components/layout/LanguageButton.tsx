01c7c4aa0505c07676481a2467a6393c44e2ce92
Leon von dem Berge <leon.vondemberge@gmail.com>
2018-12-18 10:36:56 +0200

    Remove unused variable

--- src/components/layout/LanguageButton.tsx
+++ src/components/layout/LanguageButton.tsx
@@ -3,7 +3,6 @@ import i18n from '../../i18n';
 import { DropdownItem } from 'reactstrap';
 import { setLanguage } from 'src/actions/langActions';
 import { connect } from 'react-redux';
-import { Store } from 'src/reducers';
 import { Dispatch } from 'redux';
 import { AppAction } from 'src/interfaces';
 
