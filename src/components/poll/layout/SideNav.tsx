1d68ff4c9a9da7a66ace28c70c41af5ee9548593
Leon vdb <leon.vondemberge@gmail.com>
2019-04-24 17:12:05 +0300

    initial Implementation of styled components

--- src/components/poll/layout/SideNav.tsx
+++ src/components/poll/layout/SideNav.tsx
@@ -1,26 +1,36 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 interface Props {
     pollId: string
 }
 
+const Wrapper = styled.div`
+max-width: inherit;
+width: 100%;
+height: 92vh;
+position: fixed;
+bottom: 0px;
+background: indigo;
+color: #fff;
+`
 class SideNav extends React.Component<Props> {
-    render(){
+    render() {
         return (
-            <div className="bg-secondary" style={{maxWidth: "inherit", width: "100%", height: "92vh", position: "fixed", bottom: "0px"}}>
-                                <div>
-                                    <div style={{ marginTop: "20rem", width: "100%", textAlign: "center", display: "inline-block"}}>
-                                    <Link className="unstyled-link" to={`/poll/${this.props.pollId}/`}>Overview</Link>
-                                    </div>
-                                    <div style={{ marginTop: "3rem", width: "100%", textAlign: "center"}}>
-                                    <Link className="unstyled-link" to={`/poll/${this.props.pollId}/results`}>Results</Link>
-                                    </div>
-                                    <div style={{ marginTop: "3rem", width: "100%", textAlign: "center"}}>
-                                        <Link className="unstyled-link" to={`/poll/${this.props.pollId}/settings`}>Settings</Link>
-                                    </div>
-                                </div>
-                            </div>
+            <Wrapper>
+                <div>
+                    <div style={{ marginTop: "20rem", width: "100%", textAlign: "center", display: "inline-block" }}>
+                        <Link className="unstyled-link" to={`/poll/${this.props.pollId}/`}>Overview</Link>
+                    </div>
+                    <div style={{ marginTop: "3rem", width: "100%", textAlign: "center" }}>
+                        <Link className="unstyled-link" to={`/poll/${this.props.pollId}/results`}>Results</Link>
+                    </div>
+                    <div style={{ marginTop: "3rem", width: "100%", textAlign: "center" }}>
+                        <Link className="unstyled-link" to={`/poll/${this.props.pollId}/settings`}>Settings</Link>
+                    </div>
+                </div>
+            </Wrapper>
         )
     }
 }
