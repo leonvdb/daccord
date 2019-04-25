6e4aedf3f5c0cdfd175c941d63519c535155613a
Leon vdb <leon.vondemberge@gmail.com>
2019-04-25 16:29:38 +0300

    refactor SideNav + basic style system structure

--- src/components/poll/layout/SideNav.tsx
+++ src/components/poll/layout/SideNav.tsx
@@ -1,38 +1,36 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { primary } from '../../../style/utilities/Colors'
 
 interface Props {
     pollId: string
+    className?: string
 }
 
-const Wrapper = styled.div`
+
+const SideNav = ({className, pollId}: Props) => {
+    return (
+        <div className={className}>
+            <div style={{ marginTop: "20rem", width: "100%", textAlign: "center", display: "inline-block" }}>
+                <Link className="unstyled-link" to={`/poll/${pollId}/`}>Overview</Link>
+            </div>
+            <div style={{ marginTop: "3rem", width: "100%", textAlign: "center" }}>
+                <Link className="unstyled-link" to={`/poll/${pollId}/results`}>Results</Link>
+            </div>
+            <div style={{ marginTop: "3rem", width: "100%", textAlign: "center" }}>
+                <Link className="unstyled-link" to={`/poll/${pollId}/settings`}>Settings</Link>
+            </div>
+        </div>
+    )
+}
+
+export default styled(SideNav)`
 max-width: inherit;
 width: 100%;
 height: 92vh;
 position: fixed;
 bottom: 0px;
-background: indigo;
+background: ${primary};
 color: #fff;
 `
-class SideNav extends React.Component<Props> {
-    render() {
-        return (
-            <Wrapper>
-                <div>
-                    <div style={{ marginTop: "20rem", width: "100%", textAlign: "center", display: "inline-block" }}>
-                        <Link className="unstyled-link" to={`/poll/${this.props.pollId}/`}>Overview</Link>
-                    </div>
-                    <div style={{ marginTop: "3rem", width: "100%", textAlign: "center" }}>
-                        <Link className="unstyled-link" to={`/poll/${this.props.pollId}/results`}>Results</Link>
-                    </div>
-                    <div style={{ marginTop: "3rem", width: "100%", textAlign: "center" }}>
-                        <Link className="unstyled-link" to={`/poll/${this.props.pollId}/settings`}>Settings</Link>
-                    </div>
-                </div>
-            </Wrapper>
-        )
-    }
-}
-
-export default SideNav;
---
+++ src/style/utilities/Colors.js
@@ -0,0 +1,2 @@
+export const primary = '#6448D2';
+export const primaryStrong = '#6E49FF';
