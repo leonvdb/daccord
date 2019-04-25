0c043ba37f3c643dd1139c8140e712777e7b326f
Leon vdb <leon.vondemberge@gmail.com>
2019-04-25 18:19:08 +0300

    more SideNav refactoring

--- src/components/poll/layout/SideNav.tsx
+++ src/components/poll/layout/SideNav.tsx
@@ -2,19 +2,23 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { UnstyledLink } from '../../../style/elements/Link';
 import { primary, white } from '../../../style/utilities/Colors';
+import { fixedRelativeToParent } from '../../../style/utilities/Position';
 
 interface Props {
     pollId: string
     className?: string
 }
+const PositionWrapper= styled.div`
+${fixedRelativeToParent({topInPercent: 50})}
+`
 
 
 const SideNav = ({ className, pollId }: Props) => {
     return (
         <div className={className}>
-            <div style={{ marginTop: "20rem", width: "100%", textAlign: "center", display: "inline-block" }}>
+        <PositionWrapper>
                 <UnstyledLink to={`/poll/${pollId}/`}>Overview</UnstyledLink>
-            </div>
+        </PositionWrapper>
             <div style={{ marginTop: "3rem", width: "100%", textAlign: "center" }}>
                 <UnstyledLink to={`/poll/${pollId}/results`}>Results</UnstyledLink>
             </div>
@@ -26,6 +30,7 @@ const SideNav = ({ className, pollId }: Props) => {
 }
 
 export default styled(SideNav)`
+text-align: center;
 max-width: inherit;
 width: 100%;
 height: 100%;
