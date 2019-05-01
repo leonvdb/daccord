b02b72ac0eec91748650aff074609695f57c74d1
Leon vdb <leon.vondemberge@gmail.com>
2019-05-01 13:08:57 +0300

    initial implementation of hover styling on Voting Scale

--- src/components/poll/overview/VotingScale.tsx
+++ src/components/poll/overview/VotingScale.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import styled, { css } from 'styled-components';
-import { lightGray } from '../../../style/utilities';
+import { lightGray, scale } from '../../../style/utilities';
 
 interface Props {
     className?: string
@@ -13,37 +13,39 @@ const VotingScale = (props: Props) => {
     return (
         <div className={props.className} >
             <ColoredBar current={current} />
-            <VotingNumber onMouseEnter={// tslint:disable-next-line jsx-no-lambda
-                () => { setCurrent(0) }}
+            <VotingNumber fieldNumber={0} current={current}
+                onMouseEnter={// tslint:disable-next-line jsx-no-lambda
+                    () => { setCurrent(0) }}
                 onMouseLeave={// tslint:disable-next-line jsx-no-lambda
                     () => { setCurrent(props.userRating) }}
             >0</VotingNumber>
-            <VotingNumber onMouseEnter={// tslint:disable-next-line jsx-no-lambda
-                () => { setCurrent(1) }}
+            <VotingNumber fieldNumber={1} current={current}
+                onMouseEnter={// tslint:disable-next-line jsx-no-lambda
+                    () => { setCurrent(1) }}
                 onMouseLeave={// tslint:disable-next-line jsx-no-lambda
                     () => { setCurrent(props.userRating) }}
 
             >1</VotingNumber>
-            <VotingNumber>2</VotingNumber>
-            <VotingNumber>3</VotingNumber>
-            <VotingNumber>4</VotingNumber>
-            <VotingNumber>5</VotingNumber>
-            <VotingNumber>6</VotingNumber>
-            <VotingNumber>7</VotingNumber>
-            <VotingNumber>8</VotingNumber>
-            <VotingNumber>9</VotingNumber>
-            <VotingNumber>10</VotingNumber>
+            <VotingNumber fieldNumber={2} >2</VotingNumber>
+            <VotingNumber fieldNumber={3} >3</VotingNumber>
+            <VotingNumber fieldNumber={4} >4</VotingNumber>
+            <VotingNumber fieldNumber={5} >5</VotingNumber>
+            <VotingNumber fieldNumber={6} >6</VotingNumber>
+            <VotingNumber fieldNumber={7} >7</VotingNumber>
+            <VotingNumber fieldNumber={8} >8</VotingNumber>
+            <VotingNumber fieldNumber={9} >9</VotingNumber>
+            <VotingNumber fieldNumber={10} >10</VotingNumber>
         </div>
     )
 }
 const ColoredBarStyle = {
     "0": css`
     width: 1.375rem;
-    background: #22C21E;
+    background: ${scale[0].solid};
     `,
     "1": css`
     width: ${1.375 + 1 * 1.75}rem;
-    background: linear-gradient(270deg, #51CF23 0%, rgba(81, 207, 35, 0.63) 100%);
+    background: ${scale[1].linear};
     `
 }
 
@@ -51,7 +53,12 @@ interface ColoredBarProps {
     current?: number
 }
 
-const VotingNumber = styled.span`
+interface VotingNumberProps {
+    fieldNumber: number
+    current?: number
+}
+
+const VotingNumber = styled.span<VotingNumberProps>`
 border: solid 2px ${lightGray};
 width: 1.375rem;
 height: 1.375rem;
@@ -61,10 +68,21 @@ text-align: center;
 display: table-cell;
 font-size: 0.75rem;
 font-weight: 500;
-position: relative;
 color: ${lightGray};
+position: relative;
 z-index: 1;
-`
+${({ current, fieldNumber }) => current === fieldNumber ? css`
+border: solid 2px ${scale[current].solid};
+color: ${scale[current].solid};
+font-weight: bold;
+background-color: white;
+` : current !== undefined && current > fieldNumber ? css`
+color: white;
+border: solid 2px rgba(0,0,0,0);
+` : ""}
+
+`;
+
 
 const ColoredBar = styled.div<ColoredBarProps>`
 margin-left: .375rem;
@@ -73,6 +91,7 @@ position: absolute;
 border-radius: 30px;
 width: 0;
 ${({ current }) => current !== undefined && ColoredBarStyle[current]};
+
 `
 
 export default styled(VotingScale)`
