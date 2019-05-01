e5acd46712d7d4efa410eaeccbeda5ebca1ccecc
Leon vdb <leon.vondemberge@gmail.com>
2019-05-01 13:28:09 +0300

    refactoring

--- src/components/poll/overview/VotingScale.tsx
+++ src/components/poll/overview/VotingScale.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 import styled, { css } from 'styled-components';
-import { lightGray, scale } from '../../../style/utilities';
+import { lightGray, scale, softBlack } from '../../../style/utilities';
 
 interface Props {
     className?: string
     userRating?: number
 }
 
+
 const VotingScale = (props: Props) => {
     const [current, setCurrent] = useState(props.userRating);
-    console.log(current)
     return (
         <div className={props.className} >
             <ColoredBar current={current} />
@@ -26,7 +26,12 @@ const VotingScale = (props: Props) => {
                     () => { setCurrent(props.userRating) }}
 
             >1</VotingNumber>
-            <VotingNumber fieldNumber={2} >2</VotingNumber>
+            <VotingNumber fieldNumber={2} current={current}
+                onMouseEnter={// tslint:disable-next-line jsx-no-lambda
+                    () => { setCurrent(2) }}
+                onMouseLeave={// tslint:disable-next-line jsx-no-lambda
+                    () => { setCurrent(props.userRating) }}
+            >2</VotingNumber>
             <VotingNumber fieldNumber={3} >3</VotingNumber>
             <VotingNumber fieldNumber={4} >4</VotingNumber>
             <VotingNumber fieldNumber={5} >5</VotingNumber>
@@ -38,27 +43,22 @@ const VotingScale = (props: Props) => {
         </div>
     )
 }
-const ColoredBarStyle = {
-    "0": css`
-    width: 1.375rem;
-    background: ${scale[0].solid};
-    `,
-    "1": css`
-    width: ${1.375 + 1 * 1.75}rem;
-    background: ${scale[1].linear};
-    `
-}
-
-interface ColoredBarProps {
-    current?: number
-}
 
 interface VotingNumberProps {
     fieldNumber: number
     current?: number
 }
 
+const ColoredBarStyle = (current: number) => {
+
+    return css`
+    width: ${1.375 + current * 1.75}rem;
+    background: ${scale[current].linear};
+    `
+}
+
 const VotingNumber = styled.span<VotingNumberProps>`
+cursor: pointer;
 border: solid 2px ${lightGray};
 width: 1.375rem;
 height: 1.375rem;
@@ -72,25 +72,26 @@ color: ${lightGray};
 position: relative;
 z-index: 1;
 ${({ current, fieldNumber }) => current === fieldNumber ? css`
-border: solid 2px ${scale[current].solid};
-color: ${scale[current].solid};
-font-weight: bold;
-background-color: white;
-` : current !== undefined && current > fieldNumber ? css`
-color: white;
-border: solid 2px rgba(0,0,0,0);
-` : ""}
+    border: solid 2px ${scale[current].solid};
+    color: ${softBlack};
+    font-weight: bold;
+    background-color: white;
+    ` : current !== undefined && current > fieldNumber ? css`
+    color: white;
+    border: solid 2px rgba(0,0,0,0);
+    ` : ""
+    }
 
 `;
 
 
-const ColoredBar = styled.div<ColoredBarProps>`
+const ColoredBar = styled.div<{ current?: number }>`
 margin-left: .375rem;
 height: 1.375rem;
 position: absolute;
 border-radius: 30px;
 width: 0;
-${({ current }) => current !== undefined && ColoredBarStyle[current]};
+${({ current }) => current !== undefined && ColoredBarStyle(current)};
 
 `
 
--- src/style/utilities/Colors.ts
+++ src/style/utilities/Colors.ts
@@ -1,18 +1,25 @@
 
 export const primary = '#6448D2';
 export const primaryStrong = '#6E49FF';
+
 export const white = "#fff"
-export const darkGray = "#474747";
 export const lightGray = "#E3E3E3";
+export const darkGray = "#474747";
 export const blackBlue = "#151932";
+export const softBlack = "#3E3E3E"
 export const black = "#171717";
 
 export const scale = {
     0: {
-        solid: '#22C21E'
+        solid: '#22C21E',
+        linear: '#22C21E'
     },
     1: {
         solid: '#6BD643',
         linear: 'linear-gradient(270deg, #51CF23 0%, rgba(81, 207, 35, 0.63) 100%)'
+    },
+    2: {
+        solid: '#9CE040',
+        linear: ' linear-gradient(270deg, #8CDB22 0%, rgba(140, 219, 34, 0.54) 100%);'
     }
 }
