d79a269e0c286b2b69fed89b1f85bdf28c37754c
Leon vdb <leon.vondemberge@gmail.com>
2019-05-08 16:59:12 +0300

    Finish Styling Result Details

--- src/components/poll/results/ResultDetails.tsx
+++ src/components/poll/results/ResultDetails.tsx
@@ -3,21 +3,23 @@ import { Query } from 'react-apollo';
 import { GET_VOTES_FROM_OPTION } from '../../../graphql/getOption';
 import { IOptionQuery, IUser, IPollQuery } from '../../../interfaces';
 import CircularProgressBar from '../layout/CircularProgressBar';
-import { colorScale } from '../../../style/utilities';
+import { colorScale, scale } from '../../../style/utilities';
 import styled from 'styled-components';
-import { Divider } from '../../../style/elements';
-
+import { Divider, TableCellWrapper, Label } from '../../../style/elements';
 interface Props {
     option: IOptionQuery
     poll: IPollQuery
     user: IUser
     rank: number
-    className?: string
+}
+
+interface DetailsWrapperProps {
+    userRatingColor?: string
 }
 
 const ResultDetails = (props: Props) => {
 
-    const { poll, option } = props
+    const { poll, option, rank } = props
     return <Query query={GET_VOTES_FROM_OPTION} variables={{ pollId: poll.refId, optionId: option.refId }} >
         {({ loading, error, data }) => {
             if (loading) return <p data-testid="loading-state">Loading...</p>
@@ -26,28 +28,80 @@ const ResultDetails = (props: Props) => {
             const { agreementInPercent, participationInPercent } = option.result
             const agreementColor = colorScale(100 - agreementInPercent)
             const participationColor = colorScale(100 - participationInPercent)
+            const userRatingColor: string | undefined = option.userRating !== null ? scale[option.userRating].solid : undefined
             return (
-                <div data-testid="result-details" className={props.className}>
+                <div>
+
                     <Divider />
+                    <DetailsWrapper data-testid="result-details" userRatingColor={userRatingColor}>
+                        <div className="table">
+                            <div className="table-row">
+                                <TableCellWrapper widthInPercent={5} className="rank">
+                                    {rank}
+                                </TableCellWrapper>
+                                <TableCellWrapper widthInPercent={8} className="user-rating">
+                                    {option.userRating !== null ? option.userRating : '-'}
+                                </TableCellWrapper>
+                                <TableCellWrapper widthInPercent={10}>
+                                    <CircularProgressBar sqSize={62} strokeWidth={7} percentage={agreementInPercent} color={agreementColor} />
+                                </TableCellWrapper>
+                                <TableCellWrapper widthInPercent={10}>
+                                    <CircularProgressBar sqSize={62} strokeWidth={7} percentage={participationInPercent} color={participationColor} />
+                                </TableCellWrapper>
+                                <TableCellWrapper widthInPercent={67} />
+                            </div>
+                            <div className="table-row labels">
+                                <TableCellWrapper widthInPercent={5} verticalAlign='bottom'><Label>Rank</Label></TableCellWrapper>
+                                <TableCellWrapper widthInPercent={8} verticalAlign='bottom'><Label>My Vote</Label></TableCellWrapper>
+                                <TableCellWrapper widthInPercent={10} verticalAlign='bottom'><Label>Agreement</Label></TableCellWrapper>
+                                <TableCellWrapper widthInPercent={10} verticalAlign='bottom'><Label>Participation</Label></TableCellWrapper>
+                                <TableCellWrapper widthInPercent={67} />
+                            </div>
+                        </div>
+                    </DetailsWrapper>
+
                     {/* <p>Rank: {rank}</p>
                         <p data-testid="my-vote">My Vote: {option.userRating}</p> */}
-                    <CircularProgressBar sqSize={62} strokeWidth={7} percentage={agreementInPercent} color={agreementColor} />
-                    <CircularProgressBar sqSize={62} strokeWidth={7} percentage={participationInPercent} color={participationColor} />
 
                     {/* {queryOption.votes.map(vote => {
-                            if (vote.voter.user.id !== this.props.user.id) {
-                                return <div className="bg-info" key={vote.id}>
-                                    <p>{vote.rating}</p>
-                                    <p>{vote.voter.pseudonym}</p>
-                                </div>
-                            }
-                        })} */}
+                        if (vote.voter.user.id !== this.props.user.id) {
+                            return <div className="bg-info" key={vote.id}>
+                            <p>{vote.rating}</p>
+                            <p>{vote.voter.pseudonym}</p>
+                            </div>
+                        }
+                    })} */}
                 </div>
             )
         }}
     </Query>
 }
+const DetailsWrapper = styled.div<DetailsWrapperProps>`
+padding: 1.4375rem 0;
+${TableCellWrapper}{
+    text-align: center;
+}
+${Label}{
+    margin: 0;
+}
 
-export default styled(ResultDetails)`
-font-size: 1rem;
-`;
+.labels{
+    ${TableCellWrapper}{
+    height: 2rem; 
+}
+}
+.rank{
+    font-size: 2.5rem;
+}
+.user-rating{
+    color: ${({ userRatingColor }) => userRatingColor};
+    font-weight: bold;
+    font-size: 2.5rem;
+}
+.table{
+display: table;
+margin: 0;
+}
+`
+
+export default ResultDetails;
