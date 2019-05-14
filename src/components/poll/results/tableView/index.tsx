2b0cbc1d4c5e760ad30669d0f4d5e37f445539ec
Leon vdb <leon.vondemberge@gmail.com>
2019-05-14 12:01:47 +0300

    Add Labels and arrows

--- src/components/poll/results/tableView/index.tsx
+++ src/components/poll/results/tableView/index.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { Query } from 'react-apollo';
 import { GET_INDIVIDUAL_VOTES } from '../../../../graphql/getPoll';
 import styled from 'styled-components';
-import { TableCellWrapper } from '../../../../style/elements';
+import { TableCellWrapper, Label } from '../../../../style/elements';
 import { darkGray } from '../../../../style/utilities';
 import { IPollQuery, IUser, IOptionDetails } from '../../../../interfaces';
 import ResultRow from '../ResultRow';
 import ParticipantsRow from '../ParticipantsRow';
+import Arrow from '../../../../style/elements/Arrow';
+
 
 interface Props {
     poll: IPollQuery
     pseudonym: string
     user: IUser
+    className?: string
 }
 
 const TableView = (props: Props) => {
@@ -29,34 +32,54 @@ const TableView = (props: Props) => {
         if (displayedParticipants.length >= 7) break;
     }
     return (
-        <Query query={GET_INDIVIDUAL_VOTES} variables={{ id: props.poll.refId }}>
-            {({ loading, error, data }) => {
-                if (loading) return <p>Loading...</p>
-                if (error) {
-                    console.log({ error })
-                    return <p>{error.message ? error.message.replace('GraphQL error: ', '') : 'Error :('}</p>
-                }
-                return (<OptionContainer>
-                    <TableWrapper>
-                        <ParticipantsRow displayedParticipants={displayedParticipants} />
-                        {data.poll.options.map((option: IOptionDetails) => {
-                            return <ResultRow option={option} key={`table-result-${option.refId}`} displayedParticipants={displayedParticipants} />
-                        })}
-                    </TableWrapper>
-                </OptionContainer>)
-            }}
-
-        </Query>
+        <div className={props.className}>
+            <TableWrapper className="labels">
+                <TableCellWrapper widthInPercent={29.29} verticalAlign="bottom">
+                    <Label>Option</Label>
+                </TableCellWrapper>
+                <TableCellWrapper widthInPercent={70.71} verticalAlign="bottom">
+                    <Label>Participants</Label>
+                    <div className="arrows">
+                        <Arrow active={false} />
+                        <Arrow direction="right" active={true} />
+                    </div>
+                </TableCellWrapper>
+            </TableWrapper>
+            <Query query={GET_INDIVIDUAL_VOTES} variables={{ id: props.poll.refId }}>
+                {({ loading, error, data }) => {
+                    if (loading) return <p>Loading...</p>
+                    if (error) {
+                        console.log({ error })
+                        return <p>{error.message ? error.message.replace('GraphQL error: ', '') : 'Error :('}</p>
+                    }
+                    return (<OptionContainer>
+                        <TableWrapper>
+                            <ParticipantsRow displayedParticipants={displayedParticipants} />
+                            {data.poll.options.map((option: IOptionDetails) => {
+                                return <ResultRow option={option} key={`table-result-${option.refId}`} displayedParticipants={displayedParticipants} />
+                            })}
+                        </TableWrapper>
+                    </OptionContainer>)
+                }}
+            </Query>
+        </div>
     )
 }
 
 
 const TableWrapper = styled.div`
 display: table;
-background: white;
 width: 100%;
-text-align: center;
-${TableCellWrapper}{
+`;
+
+const OptionContainer = styled.div`
+overflow: hidden;
+box-shadow: 0px 2px 8px rgba(104, 104, 104, 0.25);
+border-radius: 5px;
+${TableWrapper}{
+    background: white;
+    text-align: center;
+    ${TableCellWrapper}{
     height: 4.5625rem;
     &:first-child{
         border-left: none;
@@ -66,13 +89,24 @@ ${TableCellWrapper}{
         border-left: none;
     }
 }
+}
 `;
 
-const OptionContainer = styled.div`
-overflow: hidden;
-box-shadow: 0px 2px 8px rgba(104, 104, 104, 0.25);
-border-radius: 5px;
-`;
+export default styled(TableView)`
+.labels{
+    margin-bottom: 1.5rem; 
+    ${Label}{
+        margin-left: 1rem;
+        display: inline-block;
+    }
+    .arrows{
+        display: inline-block;
+        margin-left: 1rem;
+        svg:last-child{
+            margin-left: .3rem;
+        }
 
-export default TableView
+    }
+}
+`;
 
---
+++ src/style/elements/Arrow.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ArrowGrayLeft from '../../images/arrow-gray-left.svg';
+import SVG from 'react-inlinesvg';
+import styled, { css } from 'styled-components';
+import { primaryStrong } from '../utilities';
+
+interface Props {
+    className?: string
+    direction?: string
+    active: boolean
+}
+
+const Arrow = (props: Props) => {
+    return (
+        <Wrapper direction={props.direction} active={props.active}>
+            <SVG src={ArrowGrayLeft} />
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div<{ direction?: string, active: boolean }>`
+display: inline-block;
+svg{
+    ${({ direction }) => direction === "right" ? css`transform: rotate(180deg);` : ''}
+    circle{
+    ${({ active }) => active ? css`fill: ${primaryStrong};` : ''}
+    }
+}
+`
+
+export default Arrow;
