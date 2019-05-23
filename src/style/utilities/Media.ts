d65a6577827a02df4b7a1e941ee10a0d56160350
Leon vdb <leon.vondemberge@gmail.com>
2019-05-23 14:49:52 +0300

    update Media Queries helper

--- src/style/utilities/Media.ts
+++ src/style/utilities/Media.ts
@@ -1,11 +1,30 @@
-const customMediaQuery = (minWidth: number) =>
-    `@media (min-width: ${minWidth}px)`;
+const customMediaQuery = (minOrMax: string, breakpoint: number) =>
+    `@media (${minOrMax}-width: ${breakpoint}px)`;
+
+const sizes = {
+    xs: 0,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200
+}
 
 export const above = {
     custom: customMediaQuery,
-    xs: customMediaQuery(0),
-    sm: customMediaQuery(576),
-    md: customMediaQuery(768),
-    lg: customMediaQuery(992),
-    xl: customMediaQuery(1200),
+    xs: customMediaQuery('min', sizes.xs),
+    sm: customMediaQuery('min', sizes.sm),
+    md: customMediaQuery('min', sizes.md),
+    lg: customMediaQuery('min', sizes.lg),
+    xl: customMediaQuery('min', sizes.xl),
 };
+
+export const below = {
+    custom: customMediaQuery,
+    xs: customMediaQuery('max', sizes.xs),
+    sm: customMediaQuery('max', sizes.sm),
+    md: customMediaQuery('max', sizes.md),
+    lg: customMediaQuery('max', sizes.lg),
+    xl: customMediaQuery('max', sizes.xl),
+};
+
+
