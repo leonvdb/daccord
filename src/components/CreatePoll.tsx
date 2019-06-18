b4fe37e33b969d082f108fa2ec5437ff54fdc1e0
Leon vdb <leon.vondemberge@gmail.com>
2019-06-18 16:21:06 +0200

    Style Input area

--- src/components/CreatePoll.tsx
+++ src/components/CreatePoll.tsx
@@ -10,9 +10,13 @@ import validateEmail from '../utilities/validateEmail';
 import { Mutation } from 'react-apollo';
 import { CREATE_POLL } from '../graphql/cudPoll';
 import Close from '../images/close.svg';
-import { LargeStrongLabel } from '../style/elements';
+import { LargeStrongLabel, LargeSecondaryLabel, TernaryLabel, SecondaryButton, PrimaryButton } from '../style/elements';
+import { Flex, Box } from '@rebass/grid';
+import styled from 'styled-components';
 
-interface Props extends RouteComponentProps<any>, PropsFromDispatch, WithNamespaces { }
+interface Props extends RouteComponentProps<any>, PropsFromDispatch, WithNamespaces {
+    className?: string
+}
 
 interface Errors {
     email?: string
@@ -83,112 +87,143 @@ const CreatePoll = (props: Props) => {
 
     const { t } = props
     return (
-        <div data-testid="create-poll-form">
-            <div>
-                <button data-testid="close-button" className="nav-link float-right" type="button" // tslint:disable-next-line jsx-no-lambda
-                    onClick={() => {
-                        props.history.goBack();
-                    }}>
-                    Close <img src={Close} />
-                </button>
-            </div>
-            <LargeStrongLabel>{t("Creating a new poll")}</LargeStrongLabel>
-            <Mutation mutation={CREATE_POLL}
-                update={// tslint:disable-next-line jsx-no-lambda
-                    (cache, { data: { createPoll } }) => {
-                        const { token, user, poll, pseudonym } = createPoll
-                        props.setAuthTokenAndUser(user, token)
-                        props.setPseudonym(pseudonym)
-                        props.history.push(`/poll/${poll.refId}`)
-                    }
-                }
-            >
-                {(CREATE_POLL, { loading, error }) => {
-                    if (loading) return <div data-testid="loading-state">Loading...</div>
-                    if (error) return <div>Error :(</div>
-                    return (
-                        <form onSubmit={ // tslint:disable-next-line jsx-no-lambda
-                            (e) => { onSubmit(e, CREATE_POLL) }}>
-                            {counter === 1 && <div>
-                                <TextInputGroup
-                                    label="Title"
-                                    testId="title-input"
-                                    name="title"
-                                    placeholder="Enter Title"
-                                    value={title}
-                                    onChange={onChange}
-                                    error={errors.title}
-                                />
-                                <TextInputGroup
-                                    label="Description"
-                                    testId="description-input"
-                                    name="description"
-                                    placeholder="Enter Description"
-                                    value={description}
-                                    onChange={onChange}
-                                    error={errors.description}
-                                />
-                            </div>
+        <div data-testid="create-poll-form" className={props.className}>
+            <Flex>
+                <CloseButton>
+                    <button data-testid="close-button" type="button" // tslint:disable-next-line jsx-no-lambda
+                        onClick={() => {
+                            props.history.goBack();
+                        }}>
+                        Close <img src={Close} />
+                    </button>
+                </CloseButton>
+                <InputArea width={[1 / 2]}>
+                    <LargeStrongLabel>{t("Creating a new poll")}</LargeStrongLabel>
+                    <TernaryLabel>Step {counter}</TernaryLabel>
+                    <LargeSecondaryLabel>Information about you</LargeSecondaryLabel>
+                    <Mutation mutation={CREATE_POLL}
+                        update={// tslint:disable-next-line jsx-no-lambda
+                            (cache, { data: { createPoll } }) => {
+                                const { token, user, poll, pseudonym } = createPoll
+                                props.setAuthTokenAndUser(user, token)
+                                props.setPseudonym(pseudonym)
+                                props.history.push(`/poll/${poll.refId}`)
                             }
-                            {counter === 2 && <div>
-                                <TextInputGroup
-                                    label="Email"
-                                    testId="email-input"
-                                    name="email"
-                                    placeholder="Enter Email"
-                                    value={email}
-                                    onChange={onChange}
-                                    error={errors.email}
-                                />
-                                <TextInputGroup
-                                    label="Name"
-                                    testId="name-input"
-                                    name="name"
-                                    placeholder="Enter Name"
-                                    value={name}
-                                    onChange={onChange}
-                                    error={errors.name}
-                                />
-                            </div>
-                            }
-                            <div className="mx-auto w-50 clearfix">
-                                {counter > 1 && <button className="btn btn-secondary w-25 mt-5 d-inline-block" onClick={handlePrevious} type="button">Previous</button>}
-                                {counter === counterLimit &&
-                                    <button
-                                        className="btn btn-primary w-25 mt-5 d-inline-block float-right"
-                                        type="submit"
-                                        data-testid="finish-button" >
-                                        Finish
-                            </button>
-                                }
-                                {counter !== counterLimit &&
-                                    <button
-                                        className="btn btn-primary w-25 mt-5 d-inline-block float-right"
-                                        onClick={handleNext}
-                                        type="button"
-                                        data-testid="next-button">
-                                        Next
-                            </button>
-                                }
-                            </div>
-                        </form>
+                        }
+                    >
+                        {(CREATE_POLL, { loading, error }) => {
+                            if (loading) return <div data-testid="loading-state">Loading...</div>
+                            if (error) return <div>Error :(</div>
+                            return (
+                                <form onSubmit={ // tslint:disable-next-line jsx-no-lambda
+                                    (e) => { onSubmit(e, CREATE_POLL) }}>
+                                    {counter === 1 && <div>
+                                        <TextInputGroup
+                                            label="Title"
+                                            testId="title-input"
+                                            name="title"
+                                            placeholder="Enter Title"
+                                            value={title}
+                                            onChange={onChange}
+                                            error={errors.title}
+                                        />
+                                        <TextInputGroup
+                                            label="Description"
+                                            testId="description-input"
+                                            name="description"
+                                            placeholder="Enter Description"
+                                            value={description}
+                                            onChange={onChange}
+                                            error={errors.description}
+                                        />
+                                    </div>
+                                    }
+                                    {counter === 2 && <div>
+                                        <TextInputGroup
+                                            label="Email"
+                                            testId="email-input"
+                                            name="email"
+                                            placeholder="Enter Email"
+                                            value={email}
+                                            onChange={onChange}
+                                            error={errors.email}
+                                        />
+                                        <TextInputGroup
+                                            label="Name"
+                                            testId="name-input"
+                                            name="name"
+                                            placeholder="Enter Name"
+                                            value={name}
+                                            onChange={onChange}
+                                            error={errors.name}
+                                        />
+                                    </div>
+                                    }
 
-                    )
-                }}
-            </Mutation>
-            <span className="mx-auto mt-5 d-block text-center">
-                {counter}/{counterLimit}
-            </span>
+                                    <div>
+                                        {counter === counterLimit &&
+                                            <PrimaryButton type="submit"
+                                                data-testid="finish-button">
+                                                Finish
+                                            </PrimaryButton>
+                                        }
+                                        {counter !== counterLimit &&
+                                            <PrimaryButton
+                                                onClick={handleNext}
+                                                type="button"
+                                                data-testid="next-button">
+                                                Next
+                                        </PrimaryButton>
+                                        }
+                                        {counter > 1 && <SecondaryButton onClick={handlePrevious} type="button">Previous</SecondaryButton>}
+                                    </div>
+                                </form>
+
+                            )
+                        }}
+                    </Mutation>
+                </InputArea>
+                <Box width={[1 / 2]}>
+                    <span className="mx-auto mt-5 d-block text-center">
+                        {counter}/{counterLimit}
+                    </span>
+                </Box>
+            </Flex>
         </div>
     )
 };
 
+const CloseButton = styled.div`
+position: absolute;
+right: 0;
+`
+
+const InputArea = styled(Box)`
+margin-top: 15.5rem;
+padding: 0 6.75rem;
+`;
 
 interface PropsFromDispatch {
     setAuthTokenAndUser: (user: IUser, jwt: string) => void
     setPseudonym: (pseudonym: string) => void
 }
 
+const StyledCreatePoll = styled(CreatePoll)`
+${LargeStrongLabel}{
+    margin-bottom: 5.125rem;
+    margin-top: 0;
+}
+button{
+    float: right; 
+}
+${SecondaryButton}{
+    padding: 0.5rem 1.75rem;
+    font-size: 1.125rem;
+}
+${PrimaryButton}{
+    margin: 0;
+}
+`
 
-const ComponentWithNamespaces = withNamespaces()(CreatePoll)
+const ComponentWithNamespaces = withNamespaces()(StyledCreatePoll)
 export default connect<void, PropsFromDispatch, void>(null, { setAuthTokenAndUser, setPseudonym })(ComponentWithNamespaces);
--- src/style/elements/Headings.ts
+++ src/style/elements/Headings.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { blackBlue, black, label, heading, headingTwo, darkPurple, largeLabel, darkerGray, smallHeading, darkGray, smallLabel, smallerLabel, mediumLabel, buttonLabel, softBlack, lightRed, smallParagraph, largeStrongLabel } from '../utilities';
+import { blackBlue, black, label, heading, headingTwo, darkPurple, largeLabel, darkerGray, smallHeading, darkGray, smallLabel, smallerLabel, mediumLabel, buttonLabel, softBlack, lightRed, smallParagraph, largeStrongLabel, largeSecondaryLabel, lightishGray } from '../utilities';
 
 export const Heading = styled.h1`
 color: ${blackBlue};
@@ -27,10 +27,20 @@ color: #000;
 ${largeStrongLabel}
 `;
 
+export const LargeSecondaryLabel = styled.h1`
+color: ${blackBlue}
+${largeSecondaryLabel}
+`
+
 export const MediumLabel = styled.h3`
 color: ${black};
 ${mediumLabel}
 `;
+
+export const TernaryLabel = styled.h3`
+color: ${lightishGray}
+${headingTwo}
+`
 
 export const Label = styled.h5`
 color: #000000
