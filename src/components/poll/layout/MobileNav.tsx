56d74e93368fe75a9cc9e6444beed23234b81ddf
Leon vdb <leon.vondemberge@gmail.com>
2019-08-04 11:23:30 +0300

    Adding Logo, Invite Button and User Dropdown

--- src/components/poll/layout/MobileNav.tsx
+++ src/components/poll/layout/MobileNav.tsx
@@ -1,21 +1,62 @@
 import React from 'react'
 import styled from 'styled-components';
-import { primary } from '../../../style/utilities';
+import { primary, veryLarge } from '../../../style/utilities';
+import { UnstyledLink, ButtonLabel } from '../../../style/elements';
+import InviteIcon from '../../../images/invite-icon-white.svg'
+import { Box, Flex } from '@rebass/grid';
+import UserDropdownMenu from './UserDropdownMenu';
 
 interface IProps {
     className?: string
     isOpen: boolean
+    pseudonym: string;
 }
 
 const MobileNav = (props: IProps) => {
     return (
-        <div className={props.className} />
+        <div className={props.className} >
+            <Logo to={'/'}>Logo</Logo>
+            <FadeIn isOpen={props.isOpen}>
+                <StyledFlex>
+                    <Box width={3 / 8}>
+                        <img src={InviteIcon} />
+                    </Box>
+                    <Box width={5 / 8}>
+                        <StyledUserDropdownMenu pseudonym={props.pseudonym} />
+                    </Box>
+                </StyledFlex>
+            </FadeIn>
+        </div>
     )
 }
 
+const Logo = styled(UnstyledLink)`
+${veryLarge};
+position:relative;
+top:.5rem;
+`
+const StyledFlex = styled(Flex)`
+margin-top: 2rem;
+`
+const StyledUserDropdownMenu = styled(UserDropdownMenu)`
+${ButtonLabel}{
+color:white;
+}
+path{
+    stroke: white;
+}
+
+`
+
+const FadeIn = styled.div<{ isOpen: boolean }>`
+opacity:0;
+${({ isOpen }) => isOpen && 'opacity:1'}
+transition: opacity ease-in-out .5s;
+`
 
 export default styled(MobileNav) <{ isOpen: boolean }>`
 position: fixed;
+text-align: center;
 background: ${primary};
 width: 0;
 transition: width .4s ease-in-out;
