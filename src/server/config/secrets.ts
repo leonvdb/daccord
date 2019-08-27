8f0dc0c048ec7edc12a5203c694c9ccbb3b208b6
Leon vdb <leon.vondemberge@gmail.com>
2019-08-27 11:58:35 +0300

    Add MAIL_NAME to secrets.ts

--- src/server/config/secrets.ts
+++ src/server/config/secrets.ts
@@ -1,3 +1,4 @@
 export const mailUser = process.env.MAIL_USER;
+export const mailName = process.env.MAIL_NAME;
 export const mailPass = process.env.MAIL_PASS;
 export const secretOrKey = process.env.SECRET_OR_KEY || "Some Secret";
