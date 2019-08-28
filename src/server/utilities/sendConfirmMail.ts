e7305ac2afb9ff963fbd67d9460937cc29be6a6c
Leon vdb <leon.vondemberge@gmail.com>
2019-08-28 11:05:13 +0300

    Add different rootURL to emails

--- src/server/utilities/sendConfirmMail.ts
+++ src/server/utilities/sendConfirmMail.ts
@@ -8,13 +8,14 @@ export function sendConfirmMail(userEmail: string, poll: IPollDocument, userType
         subject: 'Error - Something went wrong',
         html: '<p>Something went horribly wrong, please contact us.<p>'
     };
+    const rootURL = process.env.ROOT_URL || "http://localhost:3000"
     switch (userType) {
         case 'createNewPoll':
             mailOptions = {
                 from: mailName,
                 to: userEmail,
                 subject: `Your new Poll "${poll.title}" has been created!`,
-                html: `<p>Click <a href="http://localhost:3000/poll/${poll.refId}?token=${token}">here</a> to get to your poll </p>` // plain text body
+                html: `<p>Click <a href="${rootURL}/poll/${poll.refId}?token=${token}">here</a> to get to your poll </p>` // plain text body
             };
             break;
         case 'becomeNewParticipant':
@@ -22,7 +23,7 @@ export function sendConfirmMail(userEmail: string, poll: IPollDocument, userType
                 from: mailName,
                 to: userEmail,
                 subject: `You became a participant of "${poll.title}"!`,
-                html: `<p>Click <a href="http://localhost:3000/poll/${poll.refId}?token=${token}">here</a> to get to the poll </p>` // plain text body
+                html: `<p>Click <a href="${rootURL}/poll/${poll.refId}?token=${token}">here</a> to get to the poll </p>` // plain text body
             };
             break;
         case 'resendExistingParticipant':
@@ -30,7 +31,7 @@ export function sendConfirmMail(userEmail: string, poll: IPollDocument, userType
                 from: mailName,
                 to: userEmail,
                 subject: `Your access link for "${poll.title}"!`,
-                html: `<p>Click <a href="http://localhost:3000/poll/${poll.refId}?token=${token}">here</a> to get to the poll </p>` // plain text body
+                html: `<p>Click <a href="${rootURL}/poll/${poll.refId}?token=${token}">here</a> to get to the poll </p>` // plain text body
             };
             break;
         case 'resendCreator':
@@ -38,7 +39,7 @@ export function sendConfirmMail(userEmail: string, poll: IPollDocument, userType
                 from: mailName,
                 to: userEmail,
                 subject: `Your access link for "${poll.title}"!`,
-                html: `<p>Click <a href="http://localhost:3000/poll/${poll.refId}?token=${token}">here</a> to get to the poll </p>` // plain text body
+                html: `<p>Click <a href="${rootURL}/poll/${poll.refId}?token=${token}">here</a> to get to the poll </p>` // plain text body
             };
             break;
         default:
